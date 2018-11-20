42b38c6a3e3b82640101eb9910c8955b8b3a9b1c
Garik Khachatryan <gvkhacha@uci.edu>
2018-11-20 13:03:59 -0800

    update demonstration

--- resources_demonstration.py
+++ resources_demonstration.py
@@ -5,24 +5,47 @@ from resources import Resources
 #admins may find it convenient to use the bot to modify resources
 
 resources = Resources()
+
 #initializing the class will give you access to .dictionary
 
-#here we create a new resource and view its contents (empty)
-#note that these methods automatically update self.dictionary by calling self.refresh
-resources.create('xtest')
-print(resources.dictionary['xtest'])
+print("Assembly resources:")
+for url in resources.get_urls("Assembly"):
+	print("\t" + url)
 
-#next we add our first url, note that this makes a backup as well
-resources.add_url('xtest', 'https://google.com')
-print(resources.dictionary['xtest'])
+print("Trying to add duplicates doesn't work")
+for url in resources.get_urls("Assembly"):
+	print(resources.add_url("Assembly", url))
 
-#make another backup, add a second url
-resources.add_url('xtest', 'malformed_urlxxxxxxxxxxxxxxx')
-print(resources.dictionary['xtest'])
+print("\n\nAssembly urls after adding")
+for url in resources.get_urls("Assembly"):
+	print("\t" + url)
 
-#restore backup and show that the contents correctly display https://g...
-resources.restore('xtest')
-print(resources.dictionary['xtest'])
+print("\nGetting urls from a resource that doesnt exist")
+print(resources.get_urls("hello!"))
 
-#Run me again and note that it excepts when trying to create 'xtest' due to exclusive write mode
+
+print("\nAdding url for a resource with no URLS yet")
+# add_urls now returns success or fail
+print(resources.add_url("hello!", "http://google.com"))
+print(resources.get_urls("hello!"))
+
+
+# 'script' used to populate json from previous files
+# targets = ["Assembly", "C++", "C", "DiscordPy", "Git", "Hacking", "Java", "Linux", "Machine", "Mobile", "Python", "Shell", "Web"]
+# for t in targets:
+# 	with open(f"Resources/{t}.txt", "r") as file:
+# 		for line in file.readlines():
+# 			resources.add_url(t.lower(), line)
+
+
+# print(resources.get_urls("Assembly"))
+# with open("Resources/Git.txt") as file:
+# 	target = "Git"
+# 	for line in file.readlines():
+# 		resources.add_url(target, line)
+
+# print(resources.get_urls("Git"))
+resources.save_file()
+
+
 
